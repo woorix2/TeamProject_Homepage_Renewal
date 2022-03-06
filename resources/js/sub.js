b1eba76b586744600f732fb641268e3987fb5660
artofchacha <artofchacha@gmail.com>
2022-03-06 22:25:37 +0900

    update qna

--- resources/js/sub.js
+++ resources/js/sub.js
@@ -1,33 +1,38 @@
 $(document).ready(function () {
+    /* tab_sec s */
+    /*탭 s*/
+    $('.tab_sec').hide();
+    $('#tab1').show();
+
+    $(document).on('click', '.tab_box a', function (e) {
+        var activeTab = $(this).attr("data-tab");
+        e.preventDefault();
+        $('.tab_box a').removeClass('on');
+        $(this).addClass('on');
+        $('html, body, div').clearQueue();
+        console.log(activeTab);
+
+        if ($('.tab_box a').hasClass('on')) {
+
+            $('.tab_sec').hide();
+            $("#" + activeTab).fadeIn();
+            $("#" + activeTab).addClass("wow fadeInDown animated");
+            $("#" + activeTab).attr("style", "visibility: visible; animation-name: fadeInDown;");
+        };
+    });
+    /*탭 e*/
+    /* tab_sec e */
     /* cscenter>location s*/
     /*링크 복사 s*/
-        var clipboard = new ClipboardJS('.location .share');
-        clipboard.on('success', function (e) {
-            alert("링크 복사가 완료되었습니다.")
-        });
-    
-        clipboard.on('error', function (e) {
-            alert("링크 복사 에러")
-        });
-        /*링크 복사 e*/
-        /*탭 s*/
-        $('.location .sec').hide();
-        $('.location #tab1').show();
-    
-        $(document).on('click', '.location .tab_box a', function (e) {
-            var activeTab = $(this).attr("data-tab");
-            e.preventDefault();
-            $('.location .tab_box a').removeClass('on');
-            $(this).addClass('on');
-    
-            console.log(activeTab);
-    
-            if ($('.location .tab_box a').hasClass('on')) {
-    
-                $('.location .sec').fadeOut();
-                $("#" + activeTab).fadeIn();
-            };
-        });
-        /*탭 e*/
+    var clipboard = new ClipboardJS('.location .share');
+    clipboard.on('success', function (e) {
+        alert("링크 복사가 완료되었습니다.")
+    });
+
+    clipboard.on('error', function (e) {
+        alert("링크 복사 에러")
+    });
+    /*링크 복사 e*/
+
     /* cscenter>location e */
 });
