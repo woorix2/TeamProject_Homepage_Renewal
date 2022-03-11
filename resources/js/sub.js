41778765e5721cebc4bd023ab7f55f2e98c93a38
이연호 <yeonho2424@gmail.com>
2022-03-11 18:59:15 +0900

    add HRshop_view_yh

--- resources/js/sub.js
+++ resources/js/sub.js
@@ -35,4 +35,59 @@ $(document).ready(function () {
     /*링크 복사 e*/
 
     /* cscenter>location e */
+
+    /* open>hrshop_view s */
+    $('.HRshop_view .slider-single').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        adaptiveHeight: true,
+        infinite: false,
+    useTransform: true,
+        speed: 400,
+        cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+    });
+
+    $('.HRshop_view .slider-nav')
+        .on('init', function(event, slick) {
+            $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+        })
+        .slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            focusOnSelect: false,
+            infinite: false,
+        //     responsive: [{
+        //         breakpoint: 500,
+        //         settings: {
+        //             slidesToShow: 4,
+        //             slidesToScroll: 4,
+        //        }
+        //     }, {
+        //         breakpoint: 420,
+        //         settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 3,
+        //    }
+        //     }]
+        });
+
+    $('.HRshop_view .slider-single').on('afterChange', function(event, slick, currentSlide) {
+        $('.slider-nav').slick('slickGoTo', currentSlide);
+        var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+        $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+        $(currrentNavSlideElem).addClass('is-active');
+    });
+
+    $('.HRshop_view .slider-nav').on('click', '.slick-slide', function(event) {
+        event.preventDefault();
+        var goToSingleSlide = $(this).data('slick-index');
+
+        $('.slider-single').slick('slickGoTo', goToSingleSlide);
+    });
+    /* open>hrshop_view e */
+    
+
 });
